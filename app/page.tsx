18841ee7496f8c44874c5bcb66d825c6dfae94aa
nishant25062002 <mainwalnishant9@gmail.com>
2025-08-04 19:29:32 +0530

    Mininum height full

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Links from "../components/Links";
 
 export default function Home() {
   return (
-    <main className="!bg-[#f4f6fa]">
+    <main className="!bg-[#f4f6fa] min-h-full">
       <Links />
     </main>
   );
