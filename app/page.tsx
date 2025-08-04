dc4045e0a747ee680699e9beb57d744e722a23a8
nishant25062002 <mainwalnishant9@gmail.com>
2025-08-04 19:32:43 +0530

    Min height to 100vh

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Links from "../components/Links";
 
 export default function Home() {
   return (
-    <main className="!bg-[#f4f6fa] min-h-full">
+    <main className="!bg-[#f4f6fa] min-h-[100vh]">
       <Links />
     </main>
   );
